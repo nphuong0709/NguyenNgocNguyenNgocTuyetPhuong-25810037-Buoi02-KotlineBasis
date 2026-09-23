98d80af28e271b6f7d72d372b0844dd7fc7931c3
Sherry <sherrynguyen.7995@gmail.com>
2026-09-23 21:23:39 +0700

    Bai 3

---
+++ app/src/main/java/com/example/nguyenngoctuyetphuong_25810037_buoi02_kotlinebasis/Bai03_TaiKhoanNganHang.kt
@@ -0,0 +1,15 @@
+package com.example.nguyenngoctuyetphuong_25810037_buoi02_kotlinebasis
+
+fun main() {
+    val soDuBanDau: Double = 30.0
+    var soDuHienTai: Double = soDuBanDau
+
+    println("So du ban dau: $soDuHienTai VND")
+
+    soDuHienTai = soDuHienTai + 20.0
+    println("Sau khi gui tien: $soDuHienTai VND")
+
+    soDuHienTai = soDuHienTai - 2.0
+    println("Sau khi rut tien: $soDuHienTai VND")
+
+}
