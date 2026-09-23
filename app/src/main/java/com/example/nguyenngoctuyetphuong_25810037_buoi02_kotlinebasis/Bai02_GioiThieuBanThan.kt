137c46e4b2718bba7fda5b66d4e6b22ebd70ee19
Sherry <sherrynguyen.7995@gmail.com>
2026-09-23 21:15:39 +0700

    Bai02_GioiThieuBanThan.kt

---
+++ app/src/main/java/com/example/nguyenngoctuyetphuong_25810037_buoi02_kotlinebasis/Bai02_GioiThieuBanThan.kt
@@ -0,0 +1,11 @@
+package com.example.nguyenngoctuyetphuong_25810037_buoi02_kotlinebasis
+
+fun main() {
+    val hoTen = "Nguyen Ngoc Tuyet Phuong"
+    val namSinh = 1995
+    val namHienTai = 2026
+
+    val tuoi = namHienTai - namSinh
+
+    println("Hi team toi la $hoTen, nam nay toi $tuoi tuoi.")
+}
