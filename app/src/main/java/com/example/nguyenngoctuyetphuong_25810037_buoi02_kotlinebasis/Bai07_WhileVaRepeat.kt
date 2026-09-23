c6921514a6ee9a0e844b433144573a47c0f04548
Sherry <sherrynguyen.7995@gmail.com>
2026-09-23 21:30:41 +0700

    bai 7

---
+++ app/src/main/java/com/example/nguyenngoctuyetphuong_25810037_buoi02_kotlinebasis/Bai07_WhileVaRepeat.kt
@@ -0,0 +1,15 @@
+package com.example.nguyenngoctuyetphuong_25810037_buoi02_kotlinebasis
+
+fun main() {
+    var so = 10
+
+    while (so >= 1) {
+        println(so)
+        so--
+    }
+
+    repeat(5) {
+        println("Hoc Kotlin")
+    }
+
+}
